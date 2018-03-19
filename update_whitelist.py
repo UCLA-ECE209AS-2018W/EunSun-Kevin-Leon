b5e8b02f06e6fda940c30a478a0833389bc990c2
DESKTOP-6Q8NGS0\EUN SUN LEE <eunsunlee@ucla.edu>
2018-03-18 18:13:58 -0700

    whitelist parser for both lan and wan

--- update_whitelist.py
+++ update_whitelist.py
@@ -6,81 +6,110 @@ import re
 import json
 import datetime
 
-def timetostr(time):
-    return time.strftime('%Y-%m-%d:%H:%M:%S')
+purge_duration = 20 #seconds = 5minutes for testing
+			
 
-def strtotime(key):
-    return datetime.strptime(key,'%Y-%m-%d:%H:%M:%S' )
+def timetostr(time):
+	return time.strftime('%Y-%m-%d:%H:%M:%S')
+
+def strtotime(time):
+	return datetime.datetime.strptime(time,'%Y-%m-%d:%H:%M:%S' )
 
 def timedifference(a,b):
-    return ((a-b).seconds)
+	return ((b-a).seconds)
+	
+def addrule(sourceip, destinationip):
+	print ("rule added")
+	# subprocess.call(["./update_rules.csh", sourceip, destinationip])
+
+#def mailnotification(subject):
 
 
-def add_whitelist(sourceip, destinationip):
-    dict_list = []
-    #print(sourceip, destinationip)
-    #open dictionary text file
-    with open(white_f, 'r') as file:
-        json_dict = file.read()
-        if (json_dict): whitelist_dict = json.loads(json_dict)
-        else: 
-            whitelist_dict = {}
-        #checks if the sourceip address exists
-        if sourceip in whitelist_dict:
-            #sourceip address exists
-            #checks if the destinationip address exists
-            if destinationip not in whitelist_dict[sourceip]:
-                whitelist_dict[sourceip].append(destinationip)
-            #print(whitelist_dict[sourceip])
-                subprocess.call(["./update_rules.csh", sourceip, destinationip])
-        else:
-#             print(sourceip, destinationip)
-            current_time =  timetostr(datetime.datetime.now())
-            dict_list = [current_time, destinationip]
-            whitelist_dict.update({sourceip:dict_list})
-            
-    	    subprocess.call(["./update_rules.csh", sourceip, destinationip])
-    with open(white_f, 'w') as file:
-        file.write(json.dumps(whitelist_dict)) # use `json.loads` to do the reverse
+def add_lan_whitelist(sourceip, destinationip):
+	dict_list = []
+	current_time = datetime.datetime.now()
+	#open dictionary text file
+	with open(white_f, 'r') as file:
+		json_dict = file.read()
+		if (json_dict): whitelist_dict = json.loads(json_dict)
+		else: 
+			whitelist_dict = {}
+		if sourceip in whitelist_dict:	#checks if the sourceip address exists
+			#sourceip address exists
+			if destinationip not in whitelist_dict[sourceip]:  #checks if the destinationip address exists
+				# time difference
+				starttime = strtotime(whitelist_dict[sourceip][0])
+				if timedifference(starttime,current_time) < purge_duration :
+					whitelist_dict[sourceip].append(destinationip)
+					addrule(sourceip, destinationip) 
+		else:
+			dict_list = [timetostr(current_time), destinationip]
+			whitelist_dict.update({sourceip:dict_list})
+			addrule(sourceip, destinationip) 
+	with open(white_f, 'w') as file:
+		file.write(json.dumps(whitelist_dict))
+	
+
+def add_wan_whitelist(sourceip, destinationip):
+	dict_list = []
+	current_time = datetime.datetime.now()
+
+	#open dictionary text file
+	with open(white_f, 'r') as file:
+		json_dict = file.read()
+		if (json_dict): whitelist_dict = json.loads(json_dict)
+		else: 
+			whitelist_dict = {}
+		if destinationip in whitelist_dict:  # checks if the destination ip address is our iot device
+			if sourceip in whitelist_dict[destinationip]: # checks if the source ip is the trusted server
+				# destination ip has talked to source ip before & Add the rule
+				if sourceip in whitelist_dict:  #sourceip address exists
+					if destinationip not in whitelist_dict[sourceip]:
+						whitelist_dict[sourceip].append(destinationip)
+						addrule(sourceip, destinationip) 
+				else:
+					dict_list = [timetostr(current_time), destinationip]
+					whitelist_dict.update({sourceip:dict_list})	
+					addrule(sourceip, destinationip)
+				
+	with open(white_f, 'w') as file:
+		file.write(json.dumps(whitelist_dict))
 	
 
 
-        
-def filterlog_search(filename, searchtext1, searchtext2):
-    h = 0 
-    returntypes  = ['date/time','rulenumber','interface','source_ip','source_port','destination_ip','destination_port','protocol']
-    with open(filename) as f:
-        for line in f:
-            if searchtext1 in line:
-                if searchtext2 in line:
-                    logline_list = [x.strip() for x in line.split(',')]
-                    if(len(logline_list) == 23):
-                        if(logline_list[0][16:34] == 'pfSense filterlog:'):
-                            date_time = logline_list[0] #date/time
-                            rulenumber = logline_list[3] #rulenumber
-                            interface = logline_list[4] #interface
-                            pass_block = logline_list[6] #pass/block
-                            source_ip = logline_list[18] #source_ip
-                            destination_ip = logline_list[19] #destination_ip
-                            protocol = logline_list[16] #protocol
-#                             print(source_ip)
-#                             if returntype == 0: output = logline_list[0] #date/time
-#                             if returntype == 1: output = logline_list[3] #rulenumber
-#                             if returntype == 2: output = logline_list[4] #interface
-#                             if returntype == 3: output = logline_list[6] #pass/block
-#                             if returntype == 4: output = logline_list[18] #source_ip
-#                             if returntype == 5: output = logline_list[19] #destination_ip
-#                             if returntype == 6: output = logline_list[16] #protocol
-                            add_whitelist(source_ip,destination_ip)
-#                             print(source_ip,destination_ip)
+		
+def filterlog_search(filename, searchtext1):
+	lansearch = 'ue0'
+	wansearch = 'em0'
+	
+	returntypes  = ['date/time','rulenumber','interface','source_ip','source_port','destination_ip','destination_port','protocol']
+	with open(filename) as f:
+		for line in f:
+			if searchtext1 in line: 
+				logline_list = [x.strip() for x in line.split(',')]
+				if(len(logline_list) == 23):
+					if(logline_list[0][16:34] == 'pfSense filterlog:'):
+						date_time = logline_list[0] #date/time
+						rulenumber = logline_list[3] #rulenumber
+						interface = logline_list[4] #interface
+						pass_block = logline_list[6] #pass/block
+						source_ip = logline_list[18] #source_ip
+						destination_ip = logline_list[19] #destination_ip
+						protocol = logline_list[16] #protocol
+						if interface == lansearch : 
+							add_lan_whitelist(source_ip,destination_ip)
+						elif interface == wansearch : 
+							add_wan_whitelist(source_ip,destination_ip)
+						
 
-
-log_f = '/var/log/filter.log'
+		
+log_f = 'testlogfile.log'
+#log_f = '/var/log/filter.log'
 white_f = 'whitelist.txt'
 
+
 searchtext1 = 'block'
-searchtext2 = 'ue0'
+#searchtext2 = 'ue0'
 #returntypes  = ['0:date/time','1:rulenumber','2:interface','3:pass/block','4:source_ip','5:destination_ip','6:protocol'
 #'7:source_port','8:destination_port']
-filterlog_search(log_f, searchtext1, searchtext2)
-
+filterlog_search(log_f, searchtext1)
